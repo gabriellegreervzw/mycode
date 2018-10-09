a84a8e5d644df452c0cb43f4e3944a1373a0abf6
Gabrielle Greer <gabrielle.greer@verizonwireless.com>
2018-10-08 19:12:48 -0500

    Testing with if

---
+++ if-test/if-hostname.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import re
+#hostname = 'MTG'
+#Changeing whatever is inputed to upper case
+hostname = input('Please enter the hostname:').upper()
+if hostname == 'MTG':
+    print('hostname matches expected config')
+print('Exiting the script.')
+
