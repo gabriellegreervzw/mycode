6332c5e52b1a07eec4cdc09de74b8346d44897a7
Gabrielle Greer <gabrielle.greer@verizonwireless.com>
2018-10-09 13:39:31 -0500

    own if-logic

---
+++ cust_if_script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#print("Select which movie you would like to select:")
+#movie_title = input('Select which movie you would like to select:'
+sequel_num = input('Select which movie title sequel you would like to view')
+
+if int(sequel_num) == 1:
+    print ("The Hobbit is told about the ring")
+elif int(sequel_num) == 2:
+    print ("There's a big battle of the two towers")
+elif int(sequel_num) == 3:
+    print("The king returns")
+else: 
+    print("This is not a valid sequel")
+  
